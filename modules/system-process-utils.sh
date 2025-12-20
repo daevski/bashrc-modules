a1db14c3b1b9f1a6ae89e4d618143878c48a38c0
David McKee <git-author.n4t20@simplelogin.com>
2025-12-20 14:58:29 -0500

    registry update, split modules

---
+++ modules/system-process-utils.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Module: system-process-utils
+# Version: 0.1.0
+# Description: Process management utilities and aliases
+# BashMod Dependencies: none
+
+alias fproc='ps aux | grep -i'
+alias psg='ps aux | grep -i'
